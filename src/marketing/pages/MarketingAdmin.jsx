835486ee8f655d9c5a696569b8f0791127e5cbc7
Enayet <enayetflweb@gamil.com>
2024-01-01 19:16:52 +0600

    marketing admin page partially done

--- src/marketing/pages/MarketingAdmin.jsx
+++ src/marketing/pages/MarketingAdmin.jsx
@@ -1,9 +1,51 @@
 import { Table } from 'flowbite-react';
 import { FaEdit } from 'react-icons/fa';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import TableLoader from '../../components/common/TableLoader';
 
 const dummyDataLead = [
+  {
+    name: 'Shafayet',
+    phone: '01638719578',
+    fbLink: 'https://facebook.com/avengers-web',
+    conversionStage: 'pending',
+    reason: 'lorem ipsum dolor sit amet.',
+    meeting: '10/11/23',
+    time: '10:21AM',
+  },
+  {
+    name: 'Shafayet',
+    phone: '01638719578',
+    fbLink: 'https://facebook.com/avengers-web',
+    conversionStage: 'pending',
+    reason:
+      'lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet.lorem ipsum dolor sit amet.',
+    meeting: '10/11/23',
+    time: '10:21AM',
+  },
+];
+const dummyCallerLead = [
+  {
+    name: 'Shafayet',
+    phone: '01638719578',
+    fbLink: 'https://facebook.com/avengers-web',
+    conversionStage: 'pending',
+    reason: 'lorem ipsum dolor sit amet.',
+    meeting: '10/11/23',
+    time: '10:21AM',
+  },
+  {
+    name: 'Shafayet',
+    phone: '01638719578',
+    fbLink: 'https://facebook.com/avengers-web',
+    conversionStage: 'pending',
+    reason:
+      'lorem ipsum dolor sit amet. lorem ipsum dolor sit amet.lorem ipsum dolor sit amet.lorem ipsum dolor sit amet.',
+    meeting: '10/11/23',
+    time: '10:21AM',
+  },
+];
+const dummyUserLead = [
   {
     name: 'Shafayet',
     phone: '01638719578',
@@ -28,6 +70,46 @@ const dummyDataLead = [
 const MarketingAdmin = () => {
   const [loading, setIsLoading] = useState(false);
   const [tab, setTab] = useState(0);
+  const [leadData, setLeadData] = useState([])
+  const [callerData, setCallerData] = useState([])
+  const [userData, setUserData] = useState([])
+
+  // load data 
+  useEffect(() => {
+    setIsLoading(true)
+    // Load data based on the selected tab
+    let dataToLoad = [];
+    switch(tab){
+      case 0: 
+      dataToLoad = leadData;
+      break;
+      case 1: 
+      dataToLoad = callerData;
+      break;
+      case 2: 
+      dataToLoad = userData;
+      break;
+      default:
+        break;
+    }
+
+    // Set loaded data to the respective tab
+    switch(tab){
+      case 0:
+        setLeadData(dataToLoad);
+        break;
+      case 1:
+        setCallerData(dataToLoad);
+        break;
+      case 2:
+        setUserData(dataToLoad);
+        break;
+        default:
+          break
+    }
+    setIsLoading(false)
+  },[tab, callerData, leadData, userData])
+
 
   return (
     <div className="my-12 overflow-x-auto h-[700px] md:h-auto">
@@ -52,6 +134,14 @@ const MarketingAdmin = () => {
             onClick={() => setTab(1)}
           >
             Callers
+          </h4>
+          <h4
+            className={` py-1.5 px-5 rounded-md hover:bg-primary hover:text-white duration-300 cursor-pointer ${
+              tab === 2 ? 'bg-primary text-white' : 'bg-gray-200'
+            }`}
+            onClick={() => setTab(2)}
+          >
+            User
           </h4>
         </div>
         {loading ? (
